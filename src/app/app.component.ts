45521629daad50a5a3d40a3c69148119e5d96ae3
ctquaye <ctquaye@gmail.com>
2019-04-01 00:27:17 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  templateUrl: "../app/app.Fcomponent.html",
-  styleUrls: ["../app/app.Fcomponent.css"]
+  templateUrl: "../app/app.component.html",
+  styleUrls: ["../app/app.component.css"]
 })
 export class AppComponent {
   title = "CodeSandbox";
