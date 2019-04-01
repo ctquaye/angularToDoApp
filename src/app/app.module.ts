58e36ee57310c04e1755f55adbec80bb25b75d51
ctquaye <ctquaye@gmail.com>
2019-03-31 23:28:22 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from "@angular/core";
 import { AngularFireModule } from "angularfire2";
 import { AngularFireDatabaseModule } from "angularfire2/database";
 
-import { AppComponent } from ".app/app.component";
+import { AppComponent } from "./app.component";
 import { environment } from "../environments/environment";
 import { TodoComponent } from "./todo/todo.component";
 @NgModule({
